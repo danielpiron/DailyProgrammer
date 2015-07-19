4f23ae9bb4fc0a804a0256f8f0a05b9139406c65
Daniel Piron <dannypiron@gmail.com>
2015-07-19 00:27:20 -0400

    C Version: Work in Progress

--- 222_easy_balancing-words/src/c/balance.c
+++ 222_easy_balancing-words/src/c/balance.c
@@ -15,6 +15,7 @@
 * D(4))
 **/
 
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,15 +25,53 @@ struct pivot_weight {
     int weight;
 };
 
+#define MAX_BUFFER 128
+
+int sum(int* list, int size) {
+    assert(size > 0);
+    assert(list != NULL);
+    int total = 0;
+    while(size) {
+        total += *list;
+        list++;
+        size--;
+    }
+    return total;
+}
+
 struct pivot_weight balance(char* word) {
-
-    struct ans;
-    int  word_len = strlen(word);
-    // TODO: Simplify the steps used the python version to avoid memory
-    // allocation.
-    
+    int values[MAX_BUFFER];
+    int factors[MAX_BUFFER];
+    int word_len = strlen(word);
+    /* TODO: Check that word length is not more than buffer. */
+    int pivot = 1;
+    for (int i = 0; i < word_len; i++) {
+        values[i] = (int)(word[i] - 'A');
+        factors[i] = values[i] * abs(pivot - 1);
+    }
     int l_weight, r_weight, l_shift, r_shift;
+    /* TODO: Check for off-by-one errors here */
+    l_weight = sum(factors, 1);
+    r_weight = sum(&factors[2], word_len - 3);
 
+    l_shift = sum(values, 1);
+    r_shift = sum(&values[1], word_len - 1);
+
+    struct pivot_weight ans = { 0, 0 };
+    while(pivot < word_len - 1) {
+        if (l_weight == r_weight) {
+            ans.pivot = pivot;
+            ans.weight = l_weight;
+            break;
+        }
+        else {
+            l_shift += values[pivot];
+            r_shift -= values[pivot];
+            l_weight += l_shift;
+            r_weight -= r_shift;
+        }
+        ++pivot;
+    }
     return ans;
 }
 
