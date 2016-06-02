af189230d9d6376f120585c6298024e68b9f9b56
Daniel Piron <dannypiron@gmail.com>
2016-06-02 01:40:17 -0400

    Convert server to use low-level interface
    For practice

--- 268_easy_network_and_cards-1/src/python/server.py
+++ 268_easy_network_and_cards-1/src/python/server.py
@@ -1,16 +1,12 @@
-import socketserver
-
-
-class MyTCPHandler(socketserver.BaseRequestHandler):
-
-    def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        self.request.sendall(self.data.upper())
+import socket
 
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
-    server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
-    server.serve_forever()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(1)
+        conn, addr = s.accept()
+        with conn:
+            data = conn.recv(1024)
+            conn.sendall(data.upper())
