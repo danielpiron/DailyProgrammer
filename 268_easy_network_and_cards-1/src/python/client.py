9a0702be659b4142bfe75b85c653c4989a6f10fb
Daniel Piron <dannypiron@gmail.com>
2016-06-02 02:18:12 -0400

    Super simple and probably not threadsafe heartbeat

--- 268_easy_network_and_cards-1/src/python/client.py
+++ 268_easy_network_and_cards-1/src/python/client.py
@@ -1,18 +1,13 @@
 import socket
-import sys
 
 HOST, PORT = "localhost", 9999
-data = " ".join(sys.argv[1:])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
     sock.connect((HOST, PORT))
-    sock.sendall(bytes(data + "\n", "utf-8"))
-
-    received = str(sock.recv(1024), "utf-8")
+    while True:
+        received = str(sock.recv(1024), "utf-8")
+        print(received)
 finally:
     sock.close()
-
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
--- 268_easy_network_and_cards-1/src/python/server.py
+++ 268_easy_network_and_cards-1/src/python/server.py
@@ -1,12 +1,34 @@
 import socket
+import time
+import threading
+
+client_sockets = []
+MAX_CLIENTS = 10
 
 
-if __name__ == "__main__":
+def listen():
     HOST, PORT = "localhost", 9999
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
-        s.listen(1)
-        conn, addr = s.accept()
-        with conn:
-            data = conn.recv(1024)
-            conn.sendall(data.upper())
+        s.listen(MAX_CLIENTS)
+        print('Listening on {}:{}'.format(HOST, PORT))
+        while True:
+            conn, addr = s.accept()
+            print('Got connection from {}'.format(addr))
+            client_sockets.append(conn)
+
+
+def heartbeat():
+    # TODO: Because 'listen' is in a thread, I can see a critical section
+    # occuring here resulting in the 'for' look freaking out over changes
+    # in the length of client_sockets
+    print('Beginning heartbeats')
+    while True:
+        for sock in client_sockets:
+            sock.sendall(b'HELLO')
+        time.sleep(1)
+
+if __name__ == "__main__":
+    listener = threading.Thread(target=listen)
+    listener.start()
+    heartbeat()
