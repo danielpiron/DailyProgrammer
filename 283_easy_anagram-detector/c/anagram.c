175d476d91a0f46e4a19c8cca8f93872687560d9
Daniel Piron <dannypiron@gmail.com>
2016-10-29 23:43:26 -0400

    Working C version with hard-coded file load

--- 283_easy_anagram-detector/c/anagram.c
+++ 283_easy_anagram-detector/c/anagram.c
@@ -1,14 +1,18 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define COUNT_TABLE_SIZE ((int)'z' - (int)'a' + 1)
+#define QUOTE '"'
+#define QUESTION '?'
 
 /**
  * Count occurences of alphabetic characters in a string
  **/
-void count_occurences(int count_table[], const char *string) {
+void count_occurences(int count_table[], const char *string)
+{
 	int i;
 	char c;
 	const char *p;
@@ -23,7 +27,8 @@ void count_occurences(int count_table[], const char *string) {
 /**
  * Returns 1 if the given words are anagrams.
  **/
-bool is_anagram(const char *word1, const char *word2) {
+bool is_anagram(const char *word1, const char *word2)
+{
 	int ctable1[COUNT_TABLE_SIZE];
 	int ctable2[COUNT_TABLE_SIZE];
 
@@ -33,7 +38,56 @@ bool is_anagram(const char *word1, const char *word2) {
 	return memcmp(ctable1, ctable2, COUNT_TABLE_SIZE * sizeof(ctable1[0])) == 0;
 }
 
-int main(int argc, char *argv[]) {
-	printf("wisdom & mid sow - %d\n", is_anagram("wisdom", "mid sow"));
-	printf("Reddit & Eat Dirt - %d\n", is_anagram("Reddit", "Eat Dirt"));
+
+char *next_quoted(char *s)
+{
+	char *start, *end;
+	start = strchr(s, QUOTE) + 1;
+	end = strchr(start, QUOTE);
+	*end = '\0';
+	return start;
+}
+
+/**
+ * Parse words from a line of input that looks like this:
+ * "Word1" ? "Word2"
+ **/
+bool parse_words(char **word1, char **word2, char *line)
+{
+	char *q_ptr = strchr(line, QUESTION);
+
+	if (q_ptr == NULL) {
+		return 0;
+	}
+
+	*word1 = next_quoted(line);
+	*word2 = next_quoted(q_ptr + 1);
+
+	return 1;
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	char *fmt;
+	char *line = NULL;
+	char *word1, *word2;
+	size_t len = 0;
+
+	fp = fopen("../inputs/1.txt", "r");
+	if (fp == NULL) {
+		printf("Couldn't open input\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	while (getline(&line, &len, fp) != -1) {
+		if (parse_words(&word1, &word2, line)) {
+			if (is_anagram(word1, word2))
+				fmt = "\"%s\" is an anagram of \"%s\"\n";
+			else
+				fmt = "\"%s\" is NOT an anagram of \"%s\"\n";
+			printf(fmt, word1, word2);
+		}
+	}
+	free(line);
 }
