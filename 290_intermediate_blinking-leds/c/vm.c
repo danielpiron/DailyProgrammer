bf790b8190c470e66897ae8cb3912b2612f46cd9
Daniel Piron <dannypiron@gmail.com>
2016-11-05 15:24:19 -0400

    Refactor main into main.c

--- 290_intermediate_blinking-leds/c/vm.c
+++ 290_intermediate_blinking-leds/c/vm.c
@@ -1,11 +1,9 @@
 #include <stdint.h>
 #include <stdio.h>
-#include <string.h>
 #include "vm.h"
 
 // This may be an overkill, but I wanted to practice my C program organization.
 uint8_t VM_fetch(VirtualMachine *vm) { return vm->memory[vm->registers.pc++]; }
-
 
 void VM_tick(VirtualMachine *m)
 {
@@ -65,7 +63,6 @@ void VM_dump(VirtualMachine *m) {
   printf("Port: %02X\n", m->ports[0]);
 }
 
-
 void VM_display_led(const VirtualMachine *vm, int port_number) {
   int i;
   char buffer[9];
@@ -76,48 +73,4 @@ void VM_display_led(const VirtualMachine *vm, int port_number) {
   }
   *p = '\0';
   printf("%s\n", buffer);
-}
-
-
-int main(int argc, char *argv[]) {
-  VirtualMachine vm;
-  uint8_t temp;
-  memset(&vm, 0, sizeof(vm));
-  /*   ld b,3 */
-  vm.memory[0] = OP_LDB;
-  vm.memory[1] = 3;
-/* triple: */
-/*   ld a,126 */
-  vm.memory[2] = OP_LDA;
-  vm.memory[3] = 126;
-/*   out (0),a */
-  vm.memory[4] = OP_OUTA;
-  vm.memory[5] = 0;
-/*   ld a,60 */
-  vm.memory[6] = OP_LDA;
-  vm.memory[7] = 60;
-/*   out (0),a */
-  vm.memory[8] = OP_OUTA;
-  vm.memory[9] = 0;
-/*   ld a,24 */
-  vm.memory[10] = OP_LDA;
-  vm.memory[11] = 24;
-/*   out (0),a */
-  vm.memory[12] = OP_OUTA;
-  vm.memory[13] = 0;
-/*   djnz triple  */
-  vm.memory[14] = OP_DJNZ;
-  vm.memory[15] = 2;
-  vm.memory[16] = OP_HALT;
-  vm.running = 1;
-
-
-  VM_dump(&vm);
-  while (vm.running) {
-    temp = vm.ports[0];
-    VM_tick(&vm);
-    if (vm.ports[0] != temp)
-      VM_display_led(&vm, 0);
-  }
-  VM_dump(&vm);
 }
