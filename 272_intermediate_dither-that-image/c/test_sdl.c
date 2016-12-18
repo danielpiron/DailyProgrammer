ecb0f2640d445190055922ce80e7080cb4900211
Daniel Piron <dannypiron@gmail.com>
2016-12-18 09:20:51 -0500

    Floyd-Steinberg Dithering

--- 272_intermediate_dither-that-image/c/test_sdl.c
+++ 272_intermediate_dither-that-image/c/test_sdl.c
@@ -2,6 +2,7 @@
 #include <SDL_image.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 float get_aspect_ratio(int w, int h) { return (float)w / h; }
 
@@ -102,56 +103,114 @@ SDL_Surface *greyscale_surface(SDL_Surface *surface) {
   return grey_surface;
 }
 
-SDL_Surface *dithered_surface(SDL_Surface *surface) {
-  SDL_Surface *grey_surface = NULL;
-  Uint8 *src_scanline;
-  Uint8 *dest_scanline;
-  int i, j;
-  const int WHITE = 255;
+uint8_t get_nearest_color(uint8_t in_color) {
   const int BLACK = 0;
-  grey_surface = SDL_CreateRGBSurface(0, surface->w, surface->h,
-                                         surface->format->BitsPerPixel,
-                                         surface->format->Rmask,
-                                         surface->format->Gmask,
-                                         surface->format->Bmask,
-                                         surface->format->Amask);
+  const int WHITE = 255;
+  return (in_color < 128) ? BLACK : WHITE;
+}
 
-  if (grey_surface == NULL) {
-    return NULL;
+int clamp_int(int a) {
+  if (a > 255) {
+    a = 255;
   }
+  if (a < 0) {
+    a = 0;
+  }
+  return a;
+}
+
+uint8_t *dither_image(uint8_t *image, int w, int h) {
+  /* Let's start off by allocating memory to represent the error distribution
+   * of the entire image. */
+  uint8_t *dithered = (uint8_t *)malloc(sizeof(uint8_t) * w * h);
+  int *error_table = (int *)calloc(w * h, sizeof(int));
+  int i, j;
+  /* - * 7
+   * 3 5 1 [/16] */
+  for (i = 0; i < h; ++i) {
+    for (j = 0; j < w; ++j) {
+      int pixel_index = i * w + j;
+      int oldpixel = clamp_int(image[pixel_index] + error_table[pixel_index]);
+      int newpixel = get_nearest_color(oldpixel);
+      int quant_error = oldpixel - newpixel;
+      dithered[pixel_index] = newpixel;
+      if (j < (w - 1)) {
+        error_table[pixel_index + 1] +=  quant_error * 7 / 16;
+      }
+      if (i < (h - 1)) {
+        if (j > 0) {
+          error_table[pixel_index + w - 1] += quant_error * 3 / 16;
+        }
+        error_table[pixel_index + w] += quant_error * 5 / 16;
+        if (j < (w - 1)) {
+          error_table[pixel_index + w + 1] += quant_error * 1 / 16;
+        } 
+      }
+    }
+  }
+  return dithered;
+}
+
+SDL_Surface *dithered_surface(SDL_Surface *surface) {
+  SDL_Surface *dithered_surface = NULL;
+  Uint8 *dest_scanline;
+  Uint8 *src_scanline;
+  uint8_t *dithered_image;
+  uint8_t *gray_image;
+  uint8_t *pixel;
+  int i, j;
+
+  gray_image = (uint8_t *)malloc(sizeof(uint8_t) * surface->w * surface->h);
 
   SDL_LockSurface(surface);
-  SDL_LockSurface(grey_surface);
-
   src_scanline = (Uint8 *)surface->pixels;
-  dest_scanline = (Uint8 *)grey_surface->pixels;
-
+  pixel = gray_image;
   for (i = 0; i < surface->h; i++) {
     Uint8 *src_pixels = src_scanline;
-    Uint8 *dest_pixels = dest_scanline;
-    int error = 0;
     for (j = 0; j < surface->w; j++) {
       Uint8 r, g, b;
-      int intensity, pixel_color;
       r = src_pixels[0];
       g = src_pixels[1];
       b = src_pixels[2];
-      intensity = greyscale(r, g, b) + error;
-      pixel_color = (intensity < 128) ? BLACK : WHITE;
-      error = intensity - pixel_color;
-      dest_pixels[0] = pixel_color;
-      dest_pixels[1] = pixel_color;
-      dest_pixels[2] = pixel_color;
+      *pixel = greyscale(r, g, b);
+      pixel++;
       src_pixels += 3;
-      dest_pixels += 3;
     }
-    src_scanline += surface->pitch;
-    dest_scanline += grey_surface->pitch;
+  src_scanline += surface->pitch;
+  }
+  SDL_UnlockSurface(surface);
+
+  dithered_image =  dither_image(gray_image, surface->w, surface->h);
+   free(gray_image);
+
+  dithered_surface = SDL_CreateRGBSurface(0, surface->w, surface->h,
+                                          surface->format->BitsPerPixel,
+                                          surface->format->Rmask,
+                                          surface->format->Gmask,
+                                          surface->format->Bmask,
+                                          surface->format->Amask);
+  if (dithered_surface == NULL) {
+    return NULL;
   }
 
-  SDL_UnlockSurface(surface);
-  SDL_UnlockSurface(grey_surface);
-  return grey_surface;
+  pixel = dithered_image;
+  dest_scanline = (Uint8 *)dithered_surface->pixels;
+  SDL_LockSurface(dithered_surface);
+  for (i = 0; i < surface->h; i++) {
+    Uint8 *dest_pixels = dest_scanline;
+    for (j = 0; j < surface->w; j++) {
+      int intensity = *pixel;
+      dest_pixels[0] = intensity;
+      dest_pixels[1] = intensity;
+      dest_pixels[2] = intensity;
+      pixel++;
+      dest_pixels += 3;
+    }
+  dest_scanline += dithered_surface->pitch;
+  }
+  SDL_UnlockSurface(dithered_surface);
+  free(dithered_image);
+  return dithered_surface;
 }
 
 
