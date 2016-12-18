1227b4fba5f7e9dfdb69105ecaecee06b62f33b8
Daniel Piron <dannypiron@gmail.com>
2016-12-18 00:34:32 -0500

    Produces vertical lines
    This is not what I expected. Perhaps the checkerboard pattern only
    appears when one does left-right-left-right alternating scanning.

--- 272_intermediate_dither-that-image/c/test_dither.c
+++ 272_intermediate_dither-that-image/c/test_dither.c
@@ -1,13 +1,20 @@
 #include <assert.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+#include <string.h>
 
 /**
  * Test Floyd Steinberg dither. According the definition of this algorithm,
  * a medium 'gray' image is expected to produce a checkerboard pattern when
  * converted to black and white.
  **/
+
+
+enum colors {
+  BLACK = 0,
+  WHITE = 255
+};
 
 uint8_t *gen_gray_image(int w, int h) {
   int i;
@@ -28,15 +35,50 @@ uint8_t *gen_checkerboard_image(int w, int h) {
   uint8_t *p = image;
   for (i = 0; i < h; ++i) {
     for (j = 0; j < w; ++j, ++p) {
-      *p = ((j + i) % 2) ? 0 : 255;
+      *p = ((j + i) % 2) ? BLACK : WHITE;
     }
   }
   return image;
 }
 
+uint8_t get_nearest_color(uint8_t in_color) {
+  return (in_color < 128) ? BLACK : WHITE;
+}
+
+uint8_t *dither_image(uint8_t *image, int w, int h) {
+  /* Let's start off by allocating memory to represent the error distribution
+   * of the entire image. */
+  uint8_t *dithered = (uint8_t *)malloc(sizeof(uint8_t) * w * h);
+  int *error_table = (int *)calloc(w * h, sizeof(int));
+  int i, j;
+  /* - * 7
+   * 3 5 1 [/16] */
+  for (i = 0; i < h; ++i) {
+    for (j = 0; j < w; ++j) {
+      int pixel_index = i * w + j;
+      int oldpixel = image[pixel_index];
+      int newpixel = get_nearest_color(oldpixel + error_table[pixel_index]);
+      int quant_error = oldpixel - newpixel;
+      error_table[pixel_index + 1] = quant_error * 7 / 16;
+      dithered[pixel_index] = newpixel;
+      if (i < (h - 1)) {
+        if (j > 0) {
+          error_table[pixel_index + w - 1] = quant_error * 3 / 16;
+        }
+        error_table[pixel_index + w] = quant_error * 5 / 16;
+        error_table[pixel_index + w + 1] = quant_error * 1 / 16;
+      }
+    }
+  }
+  return dithered;
+}
+
 int main(int argc, char *argv[]) {
   uint8_t *gray_image = gen_gray_image(4, 4);
   uint8_t *expected_image = gen_checkerboard_image(4, 4);
+  uint8_t *dithered_image = dither_image(gray_image, 4, 4);
+  assert(0 == memcmp(dithered_image, expected_image, sizeof(*expected_image) * 4 * 4));
+  free(dithered_image);
   free(expected_image);
   free(gray_image);
   return 0;
