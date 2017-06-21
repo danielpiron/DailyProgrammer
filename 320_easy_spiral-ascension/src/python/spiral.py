69a386c3bfe4ed91acd833145f079f67f08c99b8
Daniel Piron <dannypiron@gmail.com>
2017-06-21 15:29:56 -0400

    Add counter-clockwise support

--- 320_easy_spiral-ascension/src/python/spiral.py
+++ 320_easy_spiral-ascension/src/python/spiral.py
@@ -31,6 +31,10 @@ def format_matrix(matrix, column_divider=' '):
 
     return '\n'.join(formatted_rows)
 
+def turn_left(dx, dy):
+    ''' Returns updated x and y coordintes (in that order)
+    executing a 90 degree right turn. '''
+    return dy, -dx
 
 def turn_right(dx, dy):
     ''' Returns updated x and y coordintes (in that order)
@@ -38,23 +42,37 @@ def turn_right(dx, dy):
     return -dy, dx
 
 
-def spiral(num):
+def spiral(num, ccw=False):
     ''' Returns a string representing `num` by `num` matrix forming
     a spiral patterning beginning at the upper left-hand (0, 0) of
     the matrix. '''
     m = create_matrix(num, num)
-    # coordinates and velocities
-    x = 0
-    y = 0
+
+    # Regardless of which direction we are turning, we
+    # always begin moving rightward.
     dx = 1
     dy = 0
-    # limits of spiral
-    top = 1 
-    right = num - 1
-    bottom = num - 1
-    left = 0
+    if ccw:
+        # starting coordinates
+        x = 0
+        y = num - 1
+        # limits of spiral
+        top = 0 
+        right = num - 1
+        bottom = num - 2
+        left = 0
+        turn = turn_left
+    else:
+        # starting coordinates
+        x = 0
+        y = 0
+        # limits of spiral
+        top = 1 
+        right = num - 1
+        bottom = num - 1
+        left = 0
+        turn = turn_right
 
-    # used when changing direction
     change_direction = False
     for n in range(1, (num * num) + 1):
         m[y][x] = n
@@ -75,7 +93,7 @@ def spiral(num):
             left += 1
 
         if change_direction:
-            dx, dy = turn_right(dx, dy)
+            dx, dy = turn(dx, dy)
             change_direction = False
 
     return format_matrix(m)
@@ -91,6 +109,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Spiral Challenge')
     parser.add_argument('number', type=positive_integer, nargs='?')
+    parser.add_argument('--ccw', action='store_true')
     args = parser.parse_args()
 
-    print(spiral(args.number))
+    print(spiral(args.number, args.ccw))
