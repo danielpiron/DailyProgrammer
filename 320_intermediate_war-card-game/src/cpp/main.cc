7c79a72b04bee5c2b7a62b8c8ddbc31c40d9927c
Daniel Piron <dannypiron@gmail.com>
2017-06-24 21:53:44 -0400

    Extract function

--- 320_intermediate_war-card-game/src/cpp/main.cc
+++ 320_intermediate_war-card-game/src/cpp/main.cc
@@ -4,20 +4,23 @@
 #include <string>
 #include <vector>
 
+std::vector<int> parse_hand(const std::string &s) {
+  std::istringstream line_stream(s);
+  std::string num;
+  std::vector<int> hand;
+  while (getline(line_stream, num, ' ')) {
+    hand.push_back(std::stoi(num));
+  }
+  return hand;
+}
+
 int main() {
   std::ifstream f{"../../data/challenge-input1.txt"};
   std::string line;
 
   std::vector<std::vector<int>> hands;
   while (std::getline(f, line)) {
-    std::istringstream line_stream(line);
-
-    std::string num;
-    std::vector<int> hand;
-    while (getline(line_stream, num, ' ')) {
-      hand.push_back(std::stoi(num));
-    }
-    hands.push_back(hand);
+    hands.push_back(parse_hand(line));
   }
   return 0;
 }
